10203347ca6995abed02965c61cb7ff1aeb78fd0
danie <danie@MURBACH-NTB>
2018-12-09 11:18:31 -0200

    Cenário 1

---
+++ test-bompracredito/Cenario1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace test_bompracredito
+{
+    [TestClass]
+    public class Cenario1
+    {
+        [TestMethod]
+        public void Cen01()
+        {
+            BaseUrl URLBase = new BaseUrl();
+            Browser.CD.Navigate().GoToUrl(URLBase.URL);
+            Browser.CD.Manage().Window.Maximize();
+            Browser.CD.FindElement(By.Id("btnContinue")).Click();
+            Browser.CD.FindElement(By.Id("btnFiveThousand")).Click();
+            Browser.CD.FindElement(By.Id("btnContinue")).Click();
+            Browser.CD.FindElement(By.Id("btnEighteenTerm")).Click();
+            Browser.CD.FindElement(By.Id("btnContinue")).Click();
+            Browser.CD.FindElement(By.Id("name")).SendKeys("Rodrigo");
+            Browser.CD.FindElement(By.Id("btnContinue")).Click();
+            Browser.CD.FindElement(By.Id("name")).Clear();
+            Browser.CD.FindElement(By.Id("name")).SendKeys("Rodrigo Matheus da Rosa");
+            Browser.CD.FindElement(By.Id("btnContinue")).Click();
+            Browser.CD.FindElement(By.Id("email")).SendKeys("rrodrigomatheusdarosa");
+            Browser.CD.FindElement(By.Id("btnContinue")).Click();
+            Browser.CD.FindElement(By.Id("email")).Clear();
+            Browser.CD.FindElement(By.Id("email")).SendKeys("rrodrigomatheusdarosa@diclace.com.br");
+            Browser.CD.FindElement(By.Id("btnContinue")).Click();
+
+        }
+        [TestMethod]
+        public void Cen01Cadastro()
+        {
+        }
+
+
+    }
+}
---
+++ test-bompracredito/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace test_bompracredito
+{
+    class Browser
+    {
+        public static IWebDriver CD = new ChromeDriver();
+
+    }
+    class BaseUrl
+    {
+        public string URL = "https://demo.bompracredito.com.br/emprestimo-pessoal/";
+    }
+}
+
